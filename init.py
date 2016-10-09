11f80c653baf0f3f34c9fab646f3c5112089c17a
Josh Tait <Josh.tait3@gmail.com>
2016-10-09 20:15:06 +0100

    add multiple routes for site

--- init.py
+++ init.py
@@ -9,6 +9,10 @@ def loadIndex():
 def loadBrowse():
 	return render_template('all-shoes.html')
 
+@app.route("/shoe")
+def loadShoe():
+	return render_template('shoe-detail.html')
+
 if __name__ == "__main__":
 	app.run(host='0.0.0.0',debug=True)
 
