2482c676598bdbb32d949f67b326934653020a75
Josh Tait <josh.tait3@gmail.com>
2016-10-13 17:47:19 +0100

    configure routes and implement v. basic paramater passing between pages

--- init.py
+++ init.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json
+
 app = Flask("__name__")
 
 @app.route("/")
@@ -9,9 +10,21 @@ def loadIndex():
 def loadBrowse():
 	return render_template('all-shoes.html')
 
-@app.route("/shoe")
-def loadShoe():
-	return render_template('shoe-detail.html')
+
+@app.route("/allshoes/<name>")
+def loadSpecificCategory(name):
+	#load the lowtops.html file
+	return render_template('lowtops.html')
+
+#I can't seem to get ?id=<shoeID> to work, so I'm using a slash instead for
+#the time being
+
+@app.route("/shoe/id=<shoeID>")
+def loadSpecificShoe(shoeID):
+	#load the specifc show, but for now just print out the shoe ID
+	return render_template('shoe-detail-1.html')
+
+
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0',debug=True)
