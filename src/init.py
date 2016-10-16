b3cf63843fa7f24231d3237cb2474ccf4a746f0e
Josh Tait <josh.tait3@gmail.com>
2016-10-16 22:57:28 +0100

    add json support for a shoe

--- src/init.py
+++ src/init.py
@@ -12,37 +12,46 @@ def loadBrowse():
 	return render_template('all-shoes.html')
 
 
+
 @app.route("/allshoes/<name>")
 def loadSpecificCategory(name):
 	#load the lowtops.html file
 	return render_template('lowtops.html')
 
-#I can't seem to get ?id=<shoeID> to work, so I'm using a slash instead for
-#the time being
 
-#@app.route("/shoe/id=<shoeID>")
-#def loadSpecificShoe(shoeID):
-	#load the specifc show, but for now just print out the shoe ID
-	#return render_template('shoe-detail-1.html')
-#def loadShoeFromJson(shoeID):
-#	with open('shoes.json', 'r') as jsonData:
-        #dump the jsonData into a string
-        #shoeDump = json.dumps(jsonData)
 
-        #now load the data into a string
-	       # with open('shoes.json', "r") as f:
-        	#	shoeName = json.load(f)
-       		#	for i in shoeName:
-                #		if i['id'] == '01':
-                 #      			shoeName = i['name']
-		#			print(shoeName, file=sys.stderr)
+
+
+@app.route("/shoe/id=<shoeID>")
+def loadShoeFromJson(shoeID = None):
+    #translate shoeID into a string so we can parse json using it. Honestly so suprised that worked.
+    shoeID = "" + shoeID + ""
+    
+    #load the json file for manipulation
+    with open('shoes.json', 'r') as jsonData:
+        shoeData = json.load(jsonData)
+        
+    #now we've got the json file, get the exact values for the Shoe
+    #shoe name
+    ShoeName = shoeData[shoeID]['name']
+    shoeNameHandler = {'shoeName': ShoeName}
+    
+    #shoe colours
+    coloursAvailable = shoeData[shoeID]['colours']
+    shoeColourHandler = {'colours': coloursAvailable}
+    
+    #size range
+    coloursAvailable = shoeData[shoeID]['colours']
+    shoeColourHandler = {'colours': coloursAvailable}
+
+    return render_template('shoe-detail.html', shoe = shoeNameHandler, colour = shoeColourHandler)
 
 
 @app.errorhandler(404)
 def uhOh(error):
 	return render_template('404.html'), 404
 
+
+
 if __name__ == "__main__":
 	app.run(host='0.0.0.0',debug=True)
-
-	
