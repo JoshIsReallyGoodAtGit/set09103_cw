c5a6f49d5eff1a961d0dd32d28fda375b1128c70
Josh Tait <josh.tait3@gmail.com>
2016-10-20 14:53:56 +0100

    add error message for users trying to access data that does not exist

--- src/init.py
+++ src/init.py
@@ -11,27 +11,28 @@ def loadBrowse():
 	#feature idea: load one of the shoes from each category here, instead of hard-coding them
 	return render_template('all-shoes.html')
 
-
     
 @app.route("/browse/<category>")
 def loadSpecificCategory(category = None):
 	with open ('shoes.json', 'r') as jsonDataFile:
 		shoes = json.load(jsonDataFile)
-                
-        #THIS IS no longer A FUCKING NIGHTMARE. 
         return render_template('category.html', category = category, shoes = shoes)
         
 @app.route("/shoe/<category>/<shoeID>")
 def loadShoeFromJson(category = None, shoeID = None):
-    #translate shoeID into a string so we can parse json using it. Honestly so suprised that worked.
-    shoeID = "" + shoeID + ""
 
     #load the json file for manipulation
     with open('shoes.json', 'r') as jsonData:
-        shoeData = json.load(jsonData)
+        shoes = json.load(jsonData)
         
-        name = shoeData[shoeID]['name']
-    return render_template('shoe-detail.html', category = category, shoeID = shoeID, name=name)
+    name = shoes[shoeID]['name']
+    
+    for shoe in shoes:
+        if shoes[shoeID]['name'] == null:
+            def force404():
+                abort(404)
+                
+                return render_template('shoe-detail.html', category = category, name=name, shoes = shoes)
 
 
 
